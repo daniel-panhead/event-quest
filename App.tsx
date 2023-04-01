6fb789923580619f8ade33ca1fa387e39e71b5a7
Daniel Pan <57362494+daniel-panhead@users.noreply.github.com>
2023-04-01 16:07:20 -0700

    remove old import

--- App.tsx
+++ App.tsx
@@ -25,7 +25,6 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
-import {load} from './mapLoader';
 
 type SectionProps = PropsWithChildren<{
   title: string;

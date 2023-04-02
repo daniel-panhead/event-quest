dda9f29194f8e212b01d0e71b1837a0cf4edae7f
Minh Au <auduongminh2004@gmail.com>
2023-04-02 02:19:45 -0700

    add general layout for the form

--- organizer/src/App.tsx
+++ organizer/src/App.tsx
@@ -1,14 +1,19 @@
 import './App.css'
-import {Header, SideButton} from './Components'
+import {EventForm, Header, SideButton} from './Components'
 
 function App() {
   return (
     <div className='general'>
       <Header/>
-      <div className='sidebar'>
-        <SideButton name="Add Event" handler={() => {console.log("Adding Event")}}/>
-        <SideButton name="Edit Event" handler={() => {console.log("Edditng Event")}}/>
-        <SideButton name="Manage Event" handler={() => {console.log("Managing Event")}}/>
+      <div className='main-view'>
+        <div className='sidebar'>
+          <SideButton name="Add Event" handler={() => {console.log("Adding Event")}}/>
+          <SideButton name="Edit Event" handler={() => {console.log("Edditng Event")}}/>
+          <SideButton name="Manage Event" handler={() => {console.log("Managing Event")}}/>
+        </div>
+        <div className='editor'>
+          <EventForm/>
+        </div>
       </div>
     </div>
   )

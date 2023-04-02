9cf99278075e1084c155574aace94f4665026d58
Daniel Pan <57362494+daniel-panhead@users.noreply.github.com>
2023-04-02 04:13:06 -0700

    text reverse geocoding

--- app/mapLoader.ts
+++ app/mapLoader.ts
@@ -1,5 +1,6 @@
 import Config from 'react-native-config';
 import GeoCoder from 'react-native-geocoding';
+import {LatLng} from 'react-native-maps';
 
 GeoCoder.init(Config.GOOGLE_MAPS_API_KEY ? Config.GOOGLE_MAPS_API_KEY : '');
 
@@ -8,7 +9,7 @@ export const geocode = async (address: string) => {
   return geoResponse.results[0].geometry.location;
 };
 
-export const revGeoCode = async (lat, lng) => {
-  const geoResponse = await GeoCoder.from(lat, lng);
-  return geoResponse.results[0].
-}
+export const revGeoCode = async (latlng: LatLng) => {
+  const geoResponse = await GeoCoder.from(latlng.latitude, latlng.longitude);
+  return geoResponse.results[0].formatted_address;
+};

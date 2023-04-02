b272421b5c2ee6150f3ed8e0c1834082305fcdbe
Daniel Pan <57362494+daniel-panhead@users.noreply.github.com>
2023-04-01 18:44:52 -0700

    added overlay and identifier for markers

--- DataApi.tsx
+++ DataApi.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import {firebase} from '@react-native-firebase/database';
 
-const data = firebase.database()
+const data = firebase.database();
 export function printData() {
-    const dataRef = data.ref()
-        .on('value', snapshot => {
-            console.log("data: ", snapshot.val());
-    })
+  const dataRef = data.ref().on('value', snapshot => {
+    console.log('data: ', snapshot.val());
+  });
 }

401ef94ee34e417690451b6caaec5583b744ae7a
Minh Au <auduongminh2004@gmail.com>
2023-04-02 06:10:14 -0700

    able to add the event to the database

--- organizer/src/DataApi.tsx
+++ organizer/src/DataApi.tsx
@@ -1,5 +1,5 @@
 import { initializeApp } from '@firebase/app'
-import { getDatabase, onValue, ref, set} from '@firebase/database'
+import { getDatabase, onValue, ref, set, push} from '@firebase/database'
 import { Event } from './Types';
 
 const firebaseConfig = {
@@ -13,33 +13,43 @@ const firebaseConfig = {
     measurementId: "G-2QPRZ7R4MH"
   };
 
-// const exampleEvents: Event[] = [
-//   {
-//     organizer: 'BizTech',
-//     address: '2053 Main Mall, Vancouver, BC V6T 1Z2',
-//     title: 'ProduHacks',
-//     dateTime: '2023-04-01T09:30:00-08:00',
-//     description: 'Hackathon',
-//     mustRegister: true,
-//     price: 10,
-//     type: 'Full Day',
-//   },
-//   {
-//     organizer: 'Organizer Guys',
-//     address: '2052 Main Mall, Vancouver, BC V6T 1Z2',
-//     title: 'Fun Event',
-//     dateTime: '2023-04-01T09:30:00-08:00',
-//     description: 'Fun time',
-//     mustRegister: false,
-//     price: 10,
-//     type: 'Full Day',
-//   },
-// ];
+const exampleEvents: Event[] = [
+  {
+    organizer: 'BizTech',
+    address: 'Henry Angus Building',
+    title: 'ProduHacks',
+    startDateTime: '2023-04-01T09:30:00-08:00',
+    endDateTime: '2023-04-02T17:00:00-08:00',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    mustRegister: true,
+    price: 10,
+    type: 'Full Day',
+    url: 'https://google.com',
+  },
+  {
+    organizer: 'Organizer Guys',
+    address: 'Martha Piper Plaza',
+    title: 'Fun Event',
+    startDateTime: '2023-04-01T09:30:00-08:00',
+    endDateTime: '2023-04-01T012:30:00-08:00',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    mustRegister: false,
+    price: 10,
+    type: 'Full Day',
+    url: 'https://ubc.ca',
+  },
+
+];
   
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 const eventRef = ref(database, 'events/');
+// exampleEvents.forEach( (example: Event) => {
+//     writeData(example);
+// }) 
 let events: Event[] = [];
 onValue(eventRef, (snapshot) => {
     snapshot.forEach((childSnap) => {
@@ -55,8 +65,11 @@ function printData(): void {
     })
 }
 
-export function writeData(id: String, data: Event) {
-    set(eventRef, {
-        id: data
-    });
+export function writeData(data: Event) {
+    const newEventRef = push(eventRef)
+    set(newEventRef, data);
+}
+
+export function hello() {
+    console.log("Hello");
 }
